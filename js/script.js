79c104d569a5daac6c1b9cdff71c76f771075462
Grace <tesigrace98@gmail.com>
2019-08-04 20:52:07 +0200

    add a background image

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const el = document.querySelector("#module");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
