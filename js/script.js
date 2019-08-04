e40f298708ab2e8c8354bf49e7d482e6dbcbd2b1
Grace <tesigrace98@gmail.com>
2019-08-04 22:32:58 +0200

    add content and styling

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const el = document.querySelector("#module");
-
-el.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  el.style.backgroundPositionY = -e.offsetY + "px";
-});
