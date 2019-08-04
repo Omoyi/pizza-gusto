ade359f9b7f4ec56128f6e576bd59dc3ce94c318
Grace <tesigrace98@gmail.com>
2019-08-04 16:18:56 +0200

    delete content

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-  }
+  };
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-  }
+  };
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
+};
