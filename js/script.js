6c4861d870f6b5ab3f1e9caac296ce22c8680e37
Omoyi <tesigrace@gmail.com>
2019-08-05 19:56:59 +0200

    update html

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
+//business logic
 function ordering (size, crust, topping, number){
   this.size= size;
   this.crust= crust;
   this.topping= topping;
   this.number= number;
-}
+};
+
+//user interface logic
 $(document).ready(function(){
 
   $("form#form").submit(function(event){
-    event.preventDefault();
+
+  event.preventDefault();
+
+  $("#theorder").click(function(){
+
     var size=$("input#type").val();
     var crust=$("select#select").val();
     var toppings=$("select#sel2").val();
     var number=$("select#sel3").val();
-    if(size=="small"){
+  })
     
-    }
   });
 });
 
