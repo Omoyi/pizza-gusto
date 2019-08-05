dfc3a68105ba26280066c43b8e4e0365d7c7c26b
Omoyi <tesigrace@gmail.com>
2019-08-05 23:11:27 +0200

    write if conditions

--- js/script.js
+++ js/script.js
@@ -8,16 +8,35 @@ function ordering (size, crust, topping, number){
 
 //user interface logic
 $(document).ready(function(){
-
   $("form#form").submit(function(event){
-  event.preventDefault();
-  $("#theorder").click(function(){
-    var size=$("input#type").val();
-    var crust=$("select#select").val();
-    var toppings=$("select#sel2").val();
-    var number=$("select#sel3").val();
+    event.preventDefault();
+    $("#theorder").click(function(){
+      var size=$("input#type").val();
+      var crust=$("select#select").val();
+      var toppings=$("select#sel2").val();
+      var number=$("select#sel3").val();
+      if($("input#type").val()==="small"){
+        var amount = 700
+      }
+      else if($("input#type").val()==="Medium"){
+        var amount = 1000
+      }
+      else if($("input#type").val()==="Large"){
+        var amount = 1500
+      }
+      else{
+        alert("Do re-order please! bad typing")
+      }
+      if($("select#select").val()==="small"){
+        var amount1 = 600
+      }
+      if($("select#select").val()==="medium"){
+        var amount1 = 800
+      }
+      if($("select#select").val()==="large"){
+        var amount1 = 2000
+      }
     });
-    
   });
 });
 
