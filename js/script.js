07df9258df00c3aa49fdd6cf06fd8bb811cdd836
Omoyi <tesigrace@gmail.com>
2019-08-05 20:09:40 +0200

    add an image

--- js/script.js
+++ js/script.js
@@ -10,17 +10,13 @@ function ordering (size, crust, topping, number){
 $(document).ready(function(){
 
   $("form#form").submit(function(event){
-
   event.preventDefault();
-
   $("#theorder").click(function(){
-
     var size=$("input#type").val();
     var crust=$("select#select").val();
     var toppings=$("select#sel2").val();
     var number=$("select#sel3").val();
-  })
-    
+    });
   });
 });
 
